b944110a68941c0bc18cbf73148423dd81bb87b0
audith <audithadhikary081@gmail.com>
2026-04-16 20:21:29 +0600

    crud and models updated

--- app/crud.py
+++ app/crud.py
@@ -13,3 +13,25 @@ def get_students(db:Session):
     return db.query(models.Student).all()
 
 
+def update_student(db:Session,student_id:int,data:schemas.StudentUpdate):
+    student=db.query(models.Student).filter(models.Student.id==student_id).first()
+    if not student:
+        return {"massge":"student not exists in database"}
+    for key,value in data.dict(exclude_unset=True).items():
+        setattr(student,key,value)
+
+    db.commit()
+    db.refresh(student)
+    return student
+
+
+def delete_student(db:Session,student_id:int):
+    student=db.query(models.Student).filter(models.Student.id == student_id).first()
+    if not student:
+        return {"massage":"student nor exists"}
+    db.delete(student)
+    db.commit()
+    return student
+    
+
+
--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from .database import Base
 
 
 class Student(Base):
-    __tablename__="syudents"
+    __tablename__="students"
     id=Column(Integer,primary_key=True,index=True)
     name=Column(String,index=True)
     role=Column(String,default="student")
